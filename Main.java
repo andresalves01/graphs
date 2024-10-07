53bc3a0ca7fbb55709a3d99f7051c31325a2f7e0
andresalves01 <andre.alves2001@gmail.com>
2024-10-06 21:57:53 -0300

    main corrrected

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 import java.util.Set;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    // Instancia o grafo com lista de adjacência
-    AdjacencyList adjacencyList = new AdjacencyList(6);
 
   public static void main(String[] args) throws Exception {
     // Instancia o grafo com lista de adjacência
@@ -15,10 +12,6 @@ public class Main {
     adjacencyList.insertEdge(1, 3);
     adjacencyList.insertEdge(3, 4);
     adjacencyList.insertEdge(4, 5);
-
-    // Executa a busca de ciclo
-    System.out.println("Resultado da busca de ciclo:");
-    System.out.println(CicleSearcher.searchCicle(0, 3, adjacencyList));
 
     // Encontra e exibe as articulações no grafo
     System.out.println("\nArticulações encontradas:");
